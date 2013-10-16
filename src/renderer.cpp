e887e71a345fa7528a2ad85f9d775b56b7293d91
omochi <omochi6644@gmail.com>
2013-10-17 00:21:35 +0900

    add camera settings

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -7,7 +7,13 @@ PathTracer::PathTracer(int screen_width, int screen_height, int samples, int sup
 	, m_height(screen_height)
 	, m_samples(samples)
 	, m_supersamples(supersamples)
+	, m_camPos(50.0, 52.0, 220.0)
+	, m_camDir(0.0, -0.04, -1.0)
+	, m_camUp(0.0, 1.0, 0.0)
+	, m_nearScreenHeight(30.0)
+	, m_distToScreen(40.0)
 {
+	m_camDir.normalize();
 	m_result = new Color[m_width*m_height];
 }
 
--- src/renderer.h
+++ src/renderer.h
@@ -9,6 +9,12 @@ public:
 	PathTracer(int screen_width, int screen_height, int samples, int supersamples);
 	~PathTracer();
 
+	void SetCamera(const Vector3 &pos, const Vector3 &dir, const Vector3 &up) {
+		m_camPos = pos;
+		m_camDir = dir; m_camDir.normalize();
+		m_camUp = up;
+	}
+
 	void RenderScene(const Scene &scene);
 
 	const Color *GetResult() const {return m_result;}
@@ -19,5 +25,11 @@ private:
 	int m_samples;
 	int m_supersamples;
 
+	Vector3 m_camPos;
+	Vector3 m_camDir;
+	Vector3 m_camUp;
+	double m_nearScreenHeight;
+	double m_distToScreen;
+	
 	Color *m_result;
 };

30fb5bef1e4c4cce8d7003dd4d47faf656a17b48
Takayuki Itamochi <itamochi@logos.t.u-tokyo.ac.jp>
2013-10-29 16:40:10 +0900

    update cornell box parameters

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ public:
   void operator()(int samples, const Color *img) {
   	cerr << "save ppm file for sample " << samples << " ..." << endl;
     char name[1024];
-    sprintf(name, "result_%04d_%dx%d.ppm", samples, supersampling, supersampling);
+    sprintf(name, "result_sah_%04d_%dx%d.ppm", samples, supersampling, supersampling);
     clock_t begin,end;
     begin = clock();
     PPM::save(name, img, w, h);
@@ -31,15 +31,17 @@ public:
 
 int main(int argc, char *argv[]) {
 
-	int width = 320, height = 240;
+	int width = 640, height = 480;
 	//PathTracer renderer(width, height, 16, 4);
   SavePPM_callback callback(width, height);
-  PathTracer renderer(width, height, 4, 32, supersampling, &callback);
-	TestScene scene;
+  PathTracer renderer(width, height, 1, 1024, supersampling, &callback);
+	//TestScene scene;
+  CornellBoxScene scene;
 
 	cerr << "begin rendering..." << endl;
+  clock_t t1 = clock();
 	renderer.RenderScene(scene);
-
+  cerr << "total time = "  << (1.0/60*(clock()-t1)/CLOCKS_PER_SEC) << " (min)." << endl;
 
 	return 0;
 }
